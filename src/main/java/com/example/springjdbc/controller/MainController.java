aafd19c903211d40ee19931d8c4a8d953bcd6853
Margarita Murazyan <murazyan.margarita@gmail.com>
2019-01-30 18:35:29 +0400

    spring-jdbc simple example

--- src/main/java/com/example/springjdbc/controller/MainController.java
+++ src/main/java/com/example/springjdbc/controller/MainController.java
@@ -1,29 +1,46 @@
 package com.example.springjdbc.controller;
 
 
+import com.example.springjdbc.db.SpringJdbcConfig;
+import com.example.springjdbc.model.User;
 import com.example.springjdbc.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class MainController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private SpringJdbcConfig springJdbcConfig;
+    DataSource dataSource = springJdbcConfig.mysqlDataSource();
+
+
+    SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("user");
 
 
     @GetMapping("/")
-    public ResponseEntity count(){
+    public ResponseEntity count() {
         return ResponseEntity.ok(userRepository.getAllUser());
     }
 
     @GetMapping("/userById")
-    public ResponseEntity userById(@RequestParam(name = "id")int id){
+    public ResponseEntity userById(@RequestParam(name = "id") int id) {
         return ResponseEntity.ok(userRepository.getUserById(2));
+    }
+
+    @PostMapping("/addUser")
+    public int addUser(User user) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("nickname", user.getName());
+        return simpleJdbcInsert.execute(parameters);
     }
 
 }
